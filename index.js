4b7ae2c7972eea47a7e1dce460898fd3d6a002ed
Tom Thomas <tomt9715@gmail.com>
2020-06-05 22:20:15 -0400

    reworked all files

--- index.js
+++ index.js
@@ -1,37 +1,22 @@
-// function addMoreRows() {
-    
-//     var itemName = document.querySelector(".item-name").click();
-//     itemName
+function getTotals() {
+    var input1 = document.getElementById("input1").nodeValue;
+    var input2 = document.getElementById("input2").nodeValue;
+    var input3 = document.getElementById("input3").nodeValue;
+    var input4 = document.getElementById("input4").nodeValue;
+    var input5 = document.getElementById("input5").nodeValue;
 
-//     var priceAmount = document.querySelector(".price-amount").click();
+    var subTotal = input1 + input2 + input3 + input4 + input5;
+    document.getElementById("subtotal").innerText = subTotal;
 
-// } 
+    var total = (subTotal * 0.08875) + subTotal;
+    document.getElementById("total").innerText = total;
 
-function addRow() {
-    // Parent Element
-    var row = document.getElementById("entry-container");
+    var item1 = document.getElementById("item-input1").nodeValue; 
+    var item2 = document.getElementById("item-input2").nodeValue;
+    var item3 = document.getElementById("item-input3").nodeValue;
+    var item4 = document.getElementById("item-input4").nodeValue;
+    var item5 = document.getElementById("item-input5").nodeValue;
 
-    // New Row
-    let newRow = document.createElement("input");
-    let newRow2 = document.createElement("input");
-
-
-    row.appendChild(newRow);
-}
-
-
-
-
-
-
-
-function removeRow() {
-    // Parent Element
-    var row = document.getElementById("entry-container");
-
-    // Remove Row
-
-    row.removeChild(
-        addRow(newRow)
-    );
+    var itemTotal = item1 + item2 + item3 + item4 + item5;
+    document.getElementById("total-items").innerText = itemTotal;
 }
